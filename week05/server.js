db6216c67a53f1ea29119920dbc3bea2e637034d
HelloZJW <zjwxy080520@gmail.com>
2020-08-02 18:37:54 +0800

    添加 layout 代码

--- week05/server.js
+++ week05/server.js
@@ -9,26 +9,32 @@ http.createServer((req, res) => {
     }).on('end', () => {
         console.log(JSON.stringify(body));
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(`<html a=aa b='bb' c="cc" >
-    <head name='nn'>
-    <title>Document</title>
-    <style>
-        body div #myid {
-            width:100px;
-            background-color:#ff5000;
-        }
-        body div img {
-            width: 30px;
-            background-color:#ff1111;
-        }
-    </style>
-    </head>
-    <body>
-    <div id="test">
-        <img id="myid">123123</img>
-        <img src="src123"/>
-    </div>
-    </body>
+        res.end(`<html maaa=a attB="b">
+<head>
+<style>
+*#container{
+    width:500px;
+    height:300px;
+    display:flex;
+    background-color:rgb(255,255,255)
+}
+#container #myid{
+    width:200px;
+    height:100px;
+    background-color:rgb(255,0,0)
+}
+#container .c1{
+    flex:1;
+    background-color:rgb(0,255,0)
+}
+</style>
+</head>
+<body>
+<div id="container">
+    <div id="myid"></div>
+    <div class="c1"></div>
+</div>
+</body>
 </html>`);
     })
 }).listen(8000);
